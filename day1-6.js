9722468d77cb732fc49f4d6d9765a561ac859c6d
Russel-john3213 <russeljohnicomedez@gmail.com>
2025-10-04 16:48:32 +0800

    Asterisk Challenge(Unfinished)

---
+++ day1-6.js
@@ -0,0 +1,23 @@
+let n = +prompt("input");
+for (let i = 0; i < n; i++) {
+  asterisk = "*";
+  console.log(" " + asterisk);
+}
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+
+//  space = 5, asterisk = 1
+//  space = 4, asterisk = 3
+//  space = 3, asterisk = 5
+//  space = 2, asterisk = 7
+//  space = 1, asterisk = 9
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
